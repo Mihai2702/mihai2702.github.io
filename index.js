17900ed5840824a3725846fd7d0e92bef0e00a01
Velicu Mihai <mitzzza.nr1@gmail.com>
2023-01-05 21:42:00 +0200

    simplifed menu navigation

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ for (var i = 0; i < skills.length; i++) {
 }
 
 skillsEl.innerHTML = skillsHTML;
+
+//display home
+
+
+function displayHome() {
+   document.getElementById("skills").style.display = 'none';
+   document.getElementById("languages").style.display = 'none';
+   document.getElementById("projects").style.display = 'none';
+  document.getElementById("home").style.display = 'block';
+
+}
+function displaySkills() {
+  document.getElementById("home").style.display = 'none';
+  document.getElementById("languages").style.display = 'none';
+  document.getElementById("projects").style.display = 'none';
+  document.getElementById("skills").style.display = 'block';
+}
+displayLanguages();
+
+function displayLanguages(){
+  document.getElementById("skills").style.display = 'none';
+  document.getElementById("home").style.display = 'none';
+  document.getElementById("projects").style.display = 'none';
+  document.getElementById("languages").style.display = 'block';
+}
+function displayProjects(){
+  document.getElementById("home").style.display = 'none';
+  document.getElementById("skills").style.display = 'none';
+  document.getElementById("languages").style.display = 'none';
+  document.getElementById("projects").style.display = 'block';
+}
