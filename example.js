ed3b63eec2dfea4f683b8447908974e5d3fdaece
Velicu Mihai <mitzzza.nr1@gmail.com>
2023-01-05 19:12:32 +0200

    extract inline js into example.js

---
+++ example.js
@@ -0,0 +1,23 @@
+// comentariu
+console.info("page loaded");
+console.debug(2 * 3 + 1);
+console.warn(2 * (3 + 1));
+
+var employed = true; //boolean
+var age = 23;
+
+var user = "Mihai";
+user = user + " Aurelian";
+console.error(employed);
+//object
+var skills = ["html","css","js"]; 
+//object(json)
+var Costel ={
+  employed: false,
+  age: 69,
+};
+var Maricela ={
+  employed: false,
+  age: 88,
+};
+var jobTitle = document.getElementById("job-title");
